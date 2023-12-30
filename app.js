1d419135eca0d3401e913c37742a6bb4d4a485f6
saikaushik25 <avsk.drp@gmail.com>
2023-12-30 15:45:15 +0530

    Parcel and Browserslist included

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+import React from "react";
+import ReactDOM  from "react-dom";
+
 const content = React.createElement("div", {id: "parent"}, 
 [React.createElement("div", {id: "child1"}, 
 [React.createElement("h1", {}, "I am h1 tag"), 
 React.createElement("h2", {}, "I am h2 tag")])],
 [React.createElement("div", {id: "child2"},
-[React.createElement("h1", {}, "I am h1 tag"),
-React.createElement("h2", {}, "I am h2 tag")])]);
+[React.createElement("h1", {}, "I am Kaushik"),
+React.createElement("h2", {}, "This is my React")])]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(content);
 console.log(content);
